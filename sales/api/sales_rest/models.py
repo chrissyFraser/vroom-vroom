16335cdf7ca780e4a0d9d9cef7a8b2e4cee3ac32
Chrissy Fraser <plum5152@gmail.com>
2022-09-12 15:59:43 -0700

    feat - sales models; separate branch

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,18 @@
+from unicodedata import name
 from django.db import models
+from django.forms import
 
-# Create your models here.
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=100)
+    employeeNumber = models.IntegerField()
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=500)
+    phoneNumber = models.PhoneNumberField(_(""))
+
+
+class SaleRecord(models.Model):
+    pass
